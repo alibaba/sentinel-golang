178d32f16d68d9175c5b4174e53b0db8a7d0db7d
于海洋 <yuhy@tuya.com>
2019-07-12 15:34:58 +0800

    metric_bucket 使用固定数组

--- core/slots/statistic/data/metric_bucket.go
+++ core/slots/statistic/data/metric_bucket.go
@@ -13,80 +13,92 @@ const (
 	MetricEventSuccess
 	MetricEventError
 	MetricEventRt
+	// hack for getting length of enum
+	metricEventNum
 )
-const metricEventNum = 5
 
 /**
 MetricBucket store the metric statistic of each event
 (MetricEventPass、MetricEventBlock、MetricEventError、MetricEventSuccess、MetricEventRt)
 */
 type MetricBucket struct {
-	counter []uint64
-	minRt   uint64
+	// value of statistic
+	counters [metricEventNum]uint64
+	minRt    uint64
 }
 
-func (mb *MetricBucket) metricEvents() []MetricEventType {
-	met := make([]MetricEventType, 0, metricEventNum)
-	met = append(met, MetricEventPass)
-	met = append(met, MetricEventBlock)
-	met = append(met, MetricEventError)
-	met = append(met, MetricEventSuccess)
-	met = append(met, MetricEventRt)
-	return met
-}
-
-func newEmptyMetricBucket() MetricBucket {
+func newMetricBucket() MetricBucket {
 	mb := MetricBucket{
-		counter: make([]uint64, metricEventNum, metricEventNum),
-		minRt:   math.MaxUint64,
+		minRt: math.MaxUint64,
 	}
 	return mb
 }
 
 func (mb *MetricBucket) Add(event MetricEventType, count uint64) {
-	switch event {
-	case MetricEventPass:
-		atomic.AddUint64(&mb.counter[0], count)
-	case MetricEventBlock:
-		atomic.AddUint64(&mb.counter[1], count)
-	case MetricEventError:
-		atomic.AddUint64(&mb.counter[2], count)
-	case MetricEventSuccess:
-		atomic.AddUint64(&mb.counter[3], count)
-	case MetricEventRt:
-		atomic.AddUint64(&mb.counter[4], count)
-	default:
-		panic("unknown metric event type, " + string(event))
+	if event > metricEventNum {
+		panic("event is bigger then metricEventNum")
 	}
+	atomic.AddUint64(&mb.counters[event], count)
 }
 
 func (mb *MetricBucket) Get(event MetricEventType) uint64 {
-	switch event {
-	case MetricEventPass:
-		return atomic.LoadUint64(&mb.counter[0])
-	case MetricEventBlock:
-		return atomic.LoadUint64(&mb.counter[1])
-	case MetricEventError:
-		return atomic.LoadUint64(&mb.counter[2])
-	case MetricEventSuccess:
-		return atomic.LoadUint64(&mb.counter[3])
-	case MetricEventRt:
-		return atomic.LoadUint64(&mb.counter[4])
-	default:
-		panic("unknown metric event type, " + string(event))
+	if event > metricEventNum {
+		panic("event is bigger then metricEventNum")
 	}
+	return mb.counters[event]
+}
+
+func (mb *MetricBucket) MinRt() uint64 {
+	return mb.minRt
+}
+
+func (mb *MetricBucket) Reset() {
+	for i := 0; i < int(metricEventNum); i++ {
+		atomic.StoreUint64(&mb.counters[i], 0)
+	}
+	atomic.StoreUint64(&mb.minRt, math.MaxUint64)
+}
+
+func (mb *MetricBucket) AddPass(n uint64) {
+	mb.Add(MetricEventPass, n)
+}
+
+func (mb *MetricBucket) Pass() uint64 {
+	return mb.Get(MetricEventPass)
+}
+
+func (mb *MetricBucket) AddBlock(n uint64) {
+	mb.Add(MetricEventBlock, n)
+}
+
+func (mb *MetricBucket) Block() uint64 {
+	return mb.Get(MetricEventBlock)
+}
+
+func (mb *MetricBucket) AddSuccess(n uint64) {
+	mb.Add(MetricEventSuccess, n)
+}
+
+func (mb *MetricBucket) Success() uint64 {
+	return mb.Get(MetricEventSuccess)
+}
+
+func (mb *MetricBucket) AddError(n uint64) {
+	mb.Add(MetricEventError, n)
+}
+
+func (mb *MetricBucket) Error() uint64 {
+	return mb.Get(MetricEventError)
 }
 
 func (mb *MetricBucket) AddRt(rt uint64) {
 	mb.Add(MetricEventRt, rt)
+	// Not thread-safe, but it's okay.
 	if rt < mb.minRt {
 		mb.minRt = rt
 	}
 }
 
-func (mb *MetricBucket) Reset() {
-	for i := 0; i < metricEventNum; i++ {
-		atomic.StoreUint64(&mb.counter[i], 0)
-	}
-	mb.minRt = math.MaxUint64
+func (mb *MetricBucket) Rt() uint64 {
+	return mb.Get(MetricEventRt)
 }
