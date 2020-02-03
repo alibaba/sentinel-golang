69c12c5ab37aae3212ac675c58ca73599a866042
Eric Zhao <sczyh16@gmail.com>
2020-02-03 14:02:50 +0800

    Improve BlockError and TokenResult
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- core/base/block_error.go
+++ core/base/block_error.go
@@ -1,9 +1,12 @@
 package base
 
+// BlockError indicates the request was blocked by Sentinel.
 type BlockError struct {
 	blockType BlockType
 	blockMsg  string
-	rule      SentinelRule
+
+	rule          SentinelRule
+	snapshotValue interface{}
 }
 
 func (e *BlockError) BlockMsg() string {
@@ -14,8 +17,20 @@ func (e *BlockError) BlockType() BlockType {
 	return e.blockType
 }
 
+func (e *BlockError) TriggeredRule() SentinelRule {
+	return e.rule
+}
+
+func (e *BlockError) TriggeredValue() interface{} {
+	return e.snapshotValue
+}
+
 func NewBlockError(blockType BlockType, blockMsg string) *BlockError {
 	return &BlockError{blockType: blockType, blockMsg: blockMsg}
+}
+
+func NewBlockErrorWithCause(blockType BlockType, blockMsg string, rule SentinelRule, snapshot interface{}) *BlockError {
+	return &BlockError{blockType: blockType, blockMsg: blockMsg, rule: rule, snapshotValue: snapshot}
 }
 
 func (e *BlockError) Error() string {
