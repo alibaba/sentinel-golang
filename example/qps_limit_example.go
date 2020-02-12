438433a5dc3d4718d41704fc6efe485da31d1739
Eric Zhao <sczyh16@gmail.com>
2020-02-12 23:05:46 +0800

    Add basic examples of QPS flow control and file configuration
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

---
+++ example/qps_limit_example.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	sentinel "github.com/sentinel-group/sentinel-golang/api"
+	"github.com/sentinel-group/sentinel-golang/core/base"
+	"github.com/sentinel-group/sentinel-golang/core/flow"
+	"github.com/sentinel-group/sentinel-golang/util"
+	"log"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// We should initialize Sentinel first.
+	err := sentinel.InitDefault()
+	if err != nil {
+		log.Fatalf("Unexpected error: %+v", err)
+	}
+
+	_, err = flow.LoadRules([]*flow.FlowRule{
+		{
+			Resource:        "some-test",
+			MetricType:      flow.QPS,
+			Count:           10,
+			ControlBehavior: flow.Reject,
+		},
+	})
+	if err != nil {
+		log.Fatalf("Unexpected error: %+v", err)
+		return
+	}
+
+	ch := make(chan struct{})
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			for {
+				e, b := sentinel.Entry("some-test", sentinel.WithTrafficType(base.Inbound))
+				if b != nil {
+					// Blocked. We could get the block reason from the BlockError.
+					time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
+				} else {
+					// Passed, wrap the logic here.
+					fmt.Println(util.CurrentTimeMillis(), "passed")
+					time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
+
+					// Be sure the entry is exited finally.
+					e.Exit()
+				}
+
+			}
+		}()
+	}
+	<-ch
+}
