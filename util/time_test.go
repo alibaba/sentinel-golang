81adfcb984fead997182c1db7739b859abded024
朱书彦 <zsy619@gmail.com>
2020-02-23 14:58:19 +0800

    Add unit test for flow control

--- util/time_test.go
+++ util/time_test.go
@@ -5,12 +5,8 @@ import (
 	"testing"
 )
 
+//要考虑时区
 func TestFormatTimeMillis(t *testing.T) {
-	// timeUnix := time.Now().Unix() //已知的时间戳，精确到秒
-	// fmt.Println(timeUnix)
-	// formatTimeStr := time.Unix(timeUnix, 0).Format("2006-01-02 15:04:05")
-	// fmt.Println(formatTimeStr) //打印结果：2017-04-11 13:30:39
-
 	type args struct {
 		ts uint64
 	}
@@ -22,27 +18,23 @@ func TestFormatTimeMillis(t *testing.T) {
 		{
 			"test1",
 			args{1582421778887},
-			"2020-02-23 09:36:18",
+			"2020-02-23 01:36:18", //"2020-02-23 09:36:18",
 		}, {
 			"test2",
 			args{1577808000000},
-			"2020-01-01 00:00:00",
+			"2019-12-31 16:00:00", //"2020-01-01 00:00:00",
 		}, {
 			"test3",
 			args{1582423015000},
-			"2020-02-23 09:56:55",
-		}, {
-			"test3",
-			args{1582423015000},
-			"2020-02-23 09:56:55",
+			"2020-02-23 01:56:55", //"2020-02-23 09:56:55",
 		}, {
 			"test4",
 			args{1564382218000},
-			"2019-07-29 14:36:58",
+			"2019-07-29 06:36:58", //"2019-07-29 14:36:58",
 		}, {
 			"test5",
 			args{1582427442295},
-			"2020-02-23 11:10:42",
+			"2020-02-23 03:10:42", //"2020-02-23 11:10:42",
 		},
 	}
 	for _, tt := range tests {
@@ -70,7 +62,7 @@ func TestFormatDate(t *testing.T) {
 		}, {
 			"test2",
 			args{1577808000000},
-			"2020-01-01",
+			"2019-12-31", //"2020-01-01",
 		},
 	}
 	for _, tt := range tests {
@@ -83,7 +75,13 @@ func TestFormatDate(t *testing.T) {
 }
 
 func TestCurrentTimeMillis(t *testing.T) {
-	times := CurrentTimeMillis()
-	fmt.Println(times)
-	fmt.Println(FormatTimeMillis(times))
+	got := CurrentTimeMillis()
+	t.Logf("CurrentTimeMillis() %d", got)
+	fmt.Println(FormatTimeMillis(got))
+}
+
+func TestCurrentTimeNano(t *testing.T) {
+	got := CurrentTimeNano()
+	fmt.Println(got)
+	t.Logf("CurrentTimeNano() %d", got)
 }
