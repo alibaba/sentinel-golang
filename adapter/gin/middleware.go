69cd3f946096e6210c5f513dc81bc3f61353f68b
aerox <gorexlv@gmail.com>
2020-03-09 12:32:32 +0800

    fix 49; init commit

---
+++ adapter/gin/middleware.go
@@ -0,0 +1,39 @@
+package gin
+
+import (
+	"net/http"
+
+	sentinel "github.com/alibaba/sentinel-golang/api"
+	"github.com/alibaba/sentinel-golang/core/base"
+	"github.com/gin-gonic/gin"
+)
+
+// SentinelMiddleware
+func SentinelMiddleware(opts ...Option) gin.HandlerFunc {
+	options := evaluateOptions(opts)
+	return func(c *gin.Context) {
+		resourceName := c.Request.Method + "_" + c.Request.URL.Path
+
+		if options.resourceExtract != nil {
+			resourceName = options.resourceExtract(c)
+		}
+
+		entry, err := sentinel.Entry(
+			resourceName,
+			sentinel.WithResourceType(base.ResTypeWeb),
+			sentinel.WithTrafficType(base.Inbound),
+		)
+
+		if err != nil {
+			if options.blockFallback != nil {
+				options.blockFallback(c)
+			} else {
+				c.AbortWithStatus(http.StatusTooManyRequests)
+			}
+			return
+		}
+
+		defer entry.Exit()
+		c.Next()
+	}
+}
---
+++ adapter/gin/middleware_test.go
@@ -0,0 +1,108 @@
+package gin
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	sentinel "github.com/alibaba/sentinel-golang/api"
+	"github.com/alibaba/sentinel-golang/core/flow"
+	"github.com/bmizerany/assert"
+	"github.com/gin-gonic/gin"
+)
+
+func initSentinel(t *testing.T) {
+	err := sentinel.InitDefault()
+	if err != nil {
+		t.Fatalf("Unexpected error: %+v", err)
+	}
+
+	_, err = flow.LoadRules([]*flow.FlowRule{
+		{
+			Resource:        "GET_/ping",
+			MetricType:      flow.QPS,
+			Count:           1,
+			ControlBehavior: flow.Reject,
+		},
+		{
+			Resource:        "/ping",
+			MetricType:      flow.QPS,
+			Count:           0,
+			ControlBehavior: flow.Reject,
+		},
+	})
+	if err != nil {
+		t.Fatalf("Unexpected error: %+v", err)
+		return
+	}
+}
+
+func TestSentinelMiddleware(t *testing.T) {
+	type args struct {
+		opts []Option
+		method string
+		path string
+		handler func(ctx *gin.Context)
+		body io.Reader
+	}
+	type want struct {
+		code int
+	}
+	var (
+		tests = []struct {
+			name string
+			args args
+			want want
+		}{
+			{
+				name: "default get",
+				args: args{
+					opts: []Option{},
+					method: http.MethodGet,
+					path: "/ping",
+					handler: func(ctx *gin.Context) {
+						ctx.String(http.StatusOK, "ping")
+					},
+					body: nil,
+				},
+				want: want{
+					code:http.StatusOK,
+				},
+			},
+			{
+				name: "customize resource extract",
+				args: args{
+					opts: []Option{
+						WithResourceExtract(func(ctx *gin.Context) string {
+							return ctx.Request.URL.Path
+						}),
+					},
+					method: http.MethodGet,
+					path: "/ping",
+					handler: func(ctx *gin.Context) {
+						ctx.String(http.StatusOK, "ping")
+					},
+					body: nil,
+				},
+				want: want{
+					code:http.StatusTooManyRequests,
+				},
+			},
+		}
+	)
+	initSentinel(t)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			router := gin.New()
+			router.Use(SentinelMiddleware(tt.args.opts...))
+			router.Handle(tt.args.method, tt.args.path, tt.args.handler)
+			r := httptest.NewRequest(tt.args.method, tt.args.path, nil)
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, r)
+
+			assert.Equal(t, tt.want.code, w.Code)
+		})
+	}
+}
