29a2270c51de0d61bea743b2f6f4f38306296b84
aerox <gorexlv@gmail.com>
2020-03-15 20:02:12 +0800

    fix #52; update grpc unit test

--- adapter/grpc/client_test.go
+++ adapter/grpc/client_test.go
@@ -2,101 +2,96 @@ package grpc
 
 import (
 	"context"
+	"errors"
 	"testing"
-	"time"
 
-	sentinel "github.com/alibaba/sentinel-golang/api"
 	"github.com/alibaba/sentinel-golang/core/base"
 	"github.com/alibaba/sentinel-golang/core/flow"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	testpb "google.golang.org/grpc/test/grpc_testing"
 )
 
-func testClientInitSentinel(t *testing.T) {
-	// We should initialize Sentinel first.
-	err := sentinel.InitDefault()
-	if err != nil {
-		t.Fatalf("Unexpected error: %+v", err)
+func TestUnaryClientIntercept(t *testing.T) {
+	const errMsgFake = "fake error"
+	interceptor := SentinelUnaryClientIntercept()
+	invoker := func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn,
+	opts ...grpc.CallOption) error {
+		return errors.New(errMsgFake)
 	}
-
-	_, err = flow.LoadRules([]*flow.FlowRule{
-		{
-			Resource:        "/grpc.testing.TestService/UnaryCall",
-			MetricType:      flow.QPS,
-			Count:           1,
-			ControlBehavior: flow.Reject,
-		},
-		{
-			Resource:        "/grpc.testing.TestService/StreamingInputCall",
-			MetricType:      flow.QPS,
-			Count:           1,
-			ControlBehavior: flow.Reject,
-		},
+	method := "/grpc.testing.TestService/UnaryCall"
+	t.Run("success", func(t *testing.T) {
+		var _, err = flow.LoadRules([]*flow.FlowRule{
+			{
+				Resource:        "/grpc.testing.TestService/UnaryCall",
+				MetricType:      flow.QPS,
+				Count:           1,
+				ControlBehavior: flow.Reject,
+			},
+		})
+		assert.Nil(t, err)
+		err = interceptor(nil, method, nil, nil, nil, invoker)
+		assert.EqualError(t, err, errMsgFake)
+		t.Run("second fail", func(t *testing.T) {
+			err = interceptor(nil, method, nil, nil, nil, invoker)
+			assert.IsType(t, &base.BlockError{}, err)
+		})
 	})
-	if err != nil {
-		t.Fatalf("Unexpected error: %+v", err)
-		return
-	}
+
+	t.Run("fail", func(t *testing.T) {
+		var _, err = flow.LoadRules([]*flow.FlowRule{
+			{
+				Resource:        "/grpc.testing.TestService/UnaryCall",
+				MetricType:      flow.QPS,
+				Count:           0,
+				ControlBehavior: flow.Reject,
+			},
+		})
+		assert.Nil(t, err)
+		err = interceptor(nil, method, nil, nil, nil, invoker)
+		assert.IsType(t, &base.BlockError{}, err)
+	})
 }
 
-func TestUnaryClientIntercept(t *testing.T) {
-	testClientInitSentinel(t)
-	dopts := []grpc.DialOption{
-		grpc.WithUnaryInterceptor(SentinelUnaryClientIntercept()),
+func TestStreamClientIntercept(t *testing.T) {
+	const errMsgFake = "fake error"
+	interceptor := SentinelStreamClientIntercept()
+	streamer := func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string,
+		opts ...grpc.CallOption) (grpc.ClientStream, error) {
+		return nil, errors.New(errMsgFake)
 	}
-	ss := &stubServer{
-		unaryCall: func(ctx context.Context, in *testpb.SimpleRequest) (*testpb.SimpleResponse, error) {
-			payload, err := newPayload(testpb.PayloadType_COMPRESSABLE, 0)
-			if err != nil {
-				return nil, status.Errorf(codes.Aborted, "failed to make payload: %v", err)
-			}
+	method := "/grpc.testing.TestService/StreamingOutputCall"
+	t.Run("success", func(t *testing.T) {
+		var _, err = flow.LoadRules([]*flow.FlowRule{
+			{
+				Resource:        "/grpc.testing.TestService/StreamingOutputCall",
+				MetricType:      flow.QPS,
+				Count:           1,
+				ControlBehavior: flow.Reject,
+			},
+		})
+		assert.Nil(t, err)
+		rep, err := interceptor(nil, nil, nil, method, streamer)
+		assert.EqualError(t, err, errMsgFake)
+		assert.Nil(t, rep)
+		t.Run("second fail", func(t *testing.T) {
+			rep, err := interceptor(nil, nil, nil, method, streamer)
+			assert.IsType(t, &base.BlockError{}, err)
+			assert.Nil(t, rep)
+		})
+	})
 
-			payload.Body = []byte("unary call")
-
-			return &testpb.SimpleResponse{
-				Payload: payload,
-			}, nil
-		},
-	}
-	if err := ss.Start([]grpc.ServerOption{}, dopts...); err != nil {
-		t.Fatalf("Error starting endpoint server: %v", err)
-	}
-	defer ss.Stop()
-
-	// success on first calling
-	{
-		resp, err := ss.client.UnaryCall(context.Background(), &testpb.SimpleRequest{})
-		s, ok := status.FromError(err)
-		assert.Equal(t, true, ok)
-		assert.Equal(t, codes.OK, s.Code())
-
-		respBytes := resp.GetPayload().GetBody()
-		assert.Equal(t, "unary call", string(respBytes))
-	}
-
-	// fail on second calling when calling interval < 1 second
-	{
-		_, err := ss.client.UnaryCall(context.Background(), &testpb.SimpleRequest{})
-		s, ok := status.FromError(err)
-		assert.Equal(t, false, ok)
-		assert.Equal(t, codes.Unknown, s.Code())
-		berr, ok := err.(*base.BlockError)
-		assert.Equal(t, true, ok)
-		assert.Equal(t, "SentinelBlockException: Flow", berr.Error())
-	}
-
-	// success on third calling which in new leap window
-	{
-		time.Sleep(time.Second)
-		resp, err := ss.client.UnaryCall(context.Background(), &testpb.SimpleRequest{})
-		s, ok := status.FromError(err)
-		assert.Equal(t, true, ok)
-		assert.Equal(t, codes.OK, s.Code())
-
-		respBytes := resp.GetPayload().GetBody()
-		assert.Equal(t, "unary call", string(respBytes))
-	}
+	t.Run("fail", func(t *testing.T) {
+		var _, err = flow.LoadRules([]*flow.FlowRule{
+			{
+				Resource:        "/grpc.testing.TestService/StreamingOutputCall",
+				MetricType:      flow.QPS,
+				Count:           0,
+				ControlBehavior: flow.Reject,
+			},
+		})
+		assert.Nil(t, err)
+		rep, err := interceptor(nil, nil, nil, method, streamer)
+		assert.IsType(t, &base.BlockError{}, err)
+		assert.Nil(t, rep)
+	})
 }
