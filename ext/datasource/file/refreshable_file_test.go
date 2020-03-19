dbe21f8eeba026ecb81664ae272ec58beb73abad
louyuting <louyuting@users.noreply.github.com>
2020-03-19 13:53:07 +0800

    Improve multiple-error handling in file data-source and update test cases (#101)

--- ext/datasource/file/refreshable_file_test.go
+++ ext/datasource/file/refreshable_file_test.go
@@ -137,6 +137,40 @@ func TestRefreshableFileDataSource_doReadAndUpdate(t *testing.T) {
 			t.Errorf("Fail to delete test file, err: %+v", err)
 		}
 	})
+
+	t.Run("TestRefreshableFileDataSource_doReadAndUpdate_Multi_Handler_err", func(t *testing.T) {
+		err := prepareSystemRulesTestFile()
+		if err != nil {
+			t.Errorf("Fail to prepare test file, err: %+v", err)
+		}
+
+		s := &RefreshableFileDataSource{
+			sourceFilePath: TestSystemRulesFile,
+			closeChan:      make(chan struct{}),
+		}
+		mh1 := &datasource.MockPropertyHandler{}
+		hErr := errors.New("Handle error")
+		mh1.On("Handle", tmock.Anything).Return(hErr)
+		mh1.On("isPropertyConsistent", tmock.Anything).Return(false)
+		mh2 := &datasource.MockPropertyHandler{}
+		mh2.On("Handle", tmock.Anything).Return(nil)
+		mh2.On("isPropertyConsistent", tmock.Anything).Return(false)
+
+		s.AddPropertyHandler(mh1)
+		s.AddPropertyHandler(mh2)
+
+		err = s.doReadAndUpdate()
+
+		mh1.AssertNumberOfCalls(t, "Handle", 1)
+		mh2.AssertNumberOfCalls(t, "Handle", 1)
+
+		assert.True(t, err != nil && strings.Contains(err.Error(), hErr.Error()), "Fail to doReadAndUpdate.")
+
+		err = deleteSystemRulesTestFile()
+		if err != nil {
+			t.Errorf("Fail to delete test file, err: %+v", err)
+		}
+	})
 }
 
 func TestRefreshableFileDataSource_Close(t *testing.T) {
