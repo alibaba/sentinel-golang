36da212ccb8ceed172d4c23df9127dbd03ef12c6
Eric Zhao <sczyh16@gmail.com>
2020-06-18 15:10:42 +0800

    Polish circuit breaking and "hot spot" param flow example
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- example/hotspot_param_flow/hotspot_param_flow_example.go
+++ example/hotspot_param_flow/hotspot_param_flow_example.go
@@ -20,55 +20,33 @@ func main() {
 	var Resource = "test"
 
 	// We should initialize Sentinel first.
-	err := sentinel.Init("./hot-pramas-sentinel.yml")
+	err := sentinel.InitDefault()
 	if err != nil {
 		log.Fatalf("Unexpected error: %+v", err)
 	}
 
 	_, err = hotspot.LoadRules([]*hotspot.Rule{
 		{
-			Id:                "a1",
+			Id:                "1",
 			Resource:          Resource,
-			MetricType:        hotspot.Concurrency,
-			Behavior:          hotspot.Reject,
-			ParamIndex:        0,
-			Threshold:         100,
-			MaxQueueingTimeMs: 0,
-			BurstCount:        10,
-			DurationInSec:     1,
-			SpecificItems:     make(map[hotspot.SpecificValue]int64),
-		},
-		{
-			Id:                "a2",
-			Resource:          Resource,
-			MetricType:        hotspot.Concurrency,
-			Behavior:          hotspot.Reject,
+			MetricType:        hotspot.QPS,
+			ControlBehavior:   hotspot.Reject,
 			ParamIndex:        1,
-			Threshold:         100,
+			Threshold:         50,
 			MaxQueueingTimeMs: 0,
-			BurstCount:        10,
+			BurstCount:        0,
 			DurationInSec:     1,
-			SpecificItems:     make(map[hotspot.SpecificValue]int64),
+			SpecificItems: map[hotspot.SpecificValue]int64{
+				{ValKind: hotspot.KindInt, ValStr: "9"}: 0,
+			},
 		},
 		{
-			Id:                "a3",
+			Id:                "2",
 			Resource:          Resource,
-			MetricType:        hotspot.Concurrency,
-			Behavior:          hotspot.Reject,
+			MetricType:        hotspot.QPS,
+			ControlBehavior:   hotspot.Reject,
 			ParamIndex:        2,
-			Threshold:         100,
-			MaxQueueingTimeMs: 0,
-			BurstCount:        10,
-			DurationInSec:     1,
-			SpecificItems:     make(map[hotspot.SpecificValue]int64),
-		},
-		{
-			Id:                "a4",
-			Resource:          Resource,
-			MetricType:        hotspot.Concurrency,
-			Behavior:          hotspot.Reject,
-			ParamIndex:        3,
-			Threshold:         100,
+			Threshold:         50,
 			MaxQueueingTimeMs: 0,
 			BurstCount:        10,
 			DurationInSec:     1,
@@ -88,20 +66,18 @@ func main() {
 	sc.AddStatSlotLast(&stat.StatisticSlot{})
 	sc.AddStatSlotLast(&hotspot.ConcurrencyStatSlot{})
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		go func() {
 			for {
-				e, b := sentinel.Entry(Resource, sentinel.WithTrafficType(base.Inbound), sentinel.WithSlotChain(sc), sentinel.WithArgs(true, rand.Int()%3000, uuid.New().String(), uuid.New().String()))
+				e, b := sentinel.Entry(Resource, sentinel.WithTrafficType(base.Inbound), sentinel.WithSlotChain(sc), sentinel.WithArgs(true, rand.Uint32()%30, "sentinel", uuid.New().String()))
 				if b != nil {
 					// Blocked. We could get the block reason from the BlockError.
 					time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
-					fmt.Println(util.CurrentTimeMillis(), " blocked")
-				} else if e == nil && b == nil {
-					fmt.Println("e is ni")
+					//fmt.Println(util.CurrentTimeMillis(), "blocked")
 				} else {
 					// Passed, wrap the logic here.
-					fmt.Println(util.CurrentTimeMillis(), " passed")
-					time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
+					fmt.Println(util.CurrentTimeMillis(), "passed")
+					//time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
 					// Be sure the entry is exited finally.
 					e.Exit()
 				}
@@ -111,21 +87,18 @@ func main() {
 	}
 
 	for {
-		e, b := sentinel.Entry(Resource, sentinel.WithTrafficType(base.Inbound), sentinel.WithSlotChain(sc), sentinel.WithArgs(true, rand.Int()%3000, uuid.New().String(), uuid.New().String()))
+		e, b := sentinel.Entry(Resource, sentinel.WithTrafficType(base.Inbound), sentinel.WithSlotChain(sc), sentinel.WithArgs(false, uint32(9), "ahas", uuid.New().String()))
 		if b != nil {
 			// Blocked. We could get the block reason from the BlockError.
 			time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
-			fmt.Println(util.CurrentTimeMillis(), " blocked")
-		} else if e == nil && b == nil {
-			fmt.Println("e is ni")
+			fmt.Println(util.CurrentTimeMillis(), "blocked")
 		} else {
 			// Passed, wrap the logic here.
-			fmt.Println(util.CurrentTimeMillis(), " passed")
+			fmt.Println(util.CurrentTimeMillis(), "passed")
 			time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
 
 			// Be sure the entry is exited finally.
 			e.Exit()
 		}
-
 	}
 }
