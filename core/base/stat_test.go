3e9dc18de0457ae0e7ddfbef04fcce42460b6ff9
sanxun0325 <bbz17640380550@163.com>
2020-08-14 13:18:49 +0800

    commit

--- core/base/stat_test.go
+++ core/base/stat_test.go
@@ -22,7 +22,7 @@ func (m *StatNodeMock) GetQPS(event MetricEvent) float64 {
 
 func (m *StatNodeMock) GetPreviousQPS(event MetricEvent) float64 {
 	args := m.Called(event)
-	return float64(args.Int(0))
+	return args.Get(0).(float64)
 }
 
 func (m *StatNodeMock) GetMaxAvg(event MetricEvent) float64 {
