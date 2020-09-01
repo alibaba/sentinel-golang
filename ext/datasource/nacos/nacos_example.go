b531d88849b1ecc9df6d406fb09c609b14300b94
sanxun0325 <bbz17640380550@163.com>
2020-09-01 23:06:19 +0800

    update nacos example

--- ext/datasource/nacos/nacos_example.go
+++ ext/datasource/nacos/nacos_example.go
@@ -2,48 +2,49 @@ package nacos
 
 import (
 	"fmt"
+	"math/rand"
+	"sync/atomic"
 	"time"
 
+	sentinel "github.com/alibaba/sentinel-golang/api"
+	"github.com/alibaba/sentinel-golang/core/base"
+	"github.com/alibaba/sentinel-golang/util"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+
 	"github.com/alibaba/sentinel-golang/ext/datasource"
-	"github.com/nacos-group/nacos-sdk-go/clients/config_client"
-	"github.com/nacos-group/nacos-sdk-go/clients/nacos_client"
 	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/common/http_agent"
-	"github.com/stretchr/testify/mock"
 )
 
-var serverConfig = constant.ServerConfig{
-	ContextPath: "/nacos",
-	Port:        8848,
-	IpAddr:      "127.0.0.1",
-}
-
-var clientConfigTest = constant.ClientConfig{
-	BeatInterval:   10000,
-	TimeoutMs:      10000,
-	ListenInterval: 20000,
-}
-
-func createConfigClientTest() (*config_client.ConfigClient, error) {
-	nc := nacos_client.NacosClient{}
-	err := nc.SetServerConfig([]constant.ServerConfig{serverConfig})
-	err = nc.SetClientConfig(clientConfigTest)
-	err = nc.SetHttpAgent(&http_agent.HttpAgent{})
-	client, err := config_client.NewConfigClient(&nc)
-
-	return &client, err
+type Counter struct {
+	pass  *int64
+	block *int64
+	total *int64
 }
 
 func Example_NacosDatasource_CustomizeClient() {
-	client, err := createConfigClientTest()
+	counter := Counter{pass: new(int64), block: new(int64), total: new(int64)}
+
+	sc := []constant.ServerConfig{
+		{
+			ContextPath: "/nacos",
+			Port:        8848,
+			IpAddr:      "127.0.0.1",
+		},
+	}
+
+	cc := constant.ClientConfig{
+		TimeoutMs: 5000,
+	}
+	client, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": sc,
+		"clientConfig":  cc,
+	})
 	if err != nil {
 		fmt.Printf("Fail to create client, err: %+v", err)
 		return
 	}
-	h := &datasource.MockPropertyHandler{}
-	h.On("isPropertyConsistent", mock.Anything).Return(true)
-	h.On("Handle", mock.Anything).Return(nil)
-	nds, err := NewNacosDataSource(client, "sentinel-go", "system-rules", h)
+	h := datasource.NewFlowRulesHandler(datasource.FlowRuleJsonArrayParser)
+	nds, err := NewNacosDataSource(client, "sentinel-go", "flow", h)
 	if err != nil {
 		fmt.Printf("Fail to create nacos data source client, err: %+v", err)
 		return
@@ -53,8 +54,50 @@ func Example_NacosDatasource_CustomizeClient() {
 		fmt.Printf("Fail to initialize nacos data source client, err: %+v", err)
 		return
 	}
+	go timerTask(&counter)
+	ch := make(chan struct{})
+	for i := 0; i < 10; i++ {
+		go func() {
+			for {
+				atomic.AddInt64(counter.total, 1)
+				e, b := sentinel.Entry("some-test", sentinel.WithTrafficType(base.Inbound))
+				if b != nil {
+					atomic.AddInt64(counter.block, 1)
+					// Blocked. We could get the block reason from the BlockError.
+					time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
+				} else {
+					atomic.AddInt64(counter.pass, 1)
+					time.Sleep(time.Duration(rand.Uint64()%10) * time.Millisecond)
 
-	time.Sleep(time.Second * 10)
-	nds.Close()
-	fmt.Println("Nacos datasource is Closed")
+					// Be sure the entry is exited finally.
+					e.Exit()
+				}
+
+			}
+		}()
+	}
+	<-ch
+}
+
+//statistic print
+func timerTask(counter *Counter) {
+	fmt.Println("begin to statistic!!!")
+	var (
+		oldTotal, oldPass, oldBlock int64
+	)
+	for {
+		time.Sleep(1 * time.Second)
+		globalTotal := atomic.LoadInt64(counter.total)
+		oneSecondTotal := globalTotal - oldTotal
+		oldTotal = globalTotal
+
+		globalPass := atomic.LoadInt64(counter.pass)
+		oneSecondPass := globalPass - oldPass
+		oldPass = globalPass
+
+		globalBlock := atomic.LoadInt64(counter.block)
+		oneSecondBlock := globalBlock - oldBlock
+		oldBlock = globalBlock
+		fmt.Println(util.CurrentTimeMillis()/1000, "total:", oneSecondTotal, " pass:", oneSecondPass, " block:", oneSecondBlock)
+	}
 }
