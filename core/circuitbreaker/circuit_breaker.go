b4c57d41fab60f7c91e539508f7698f139bcb477
louyuting <1849491904@qq.com>
2020-11-05 13:00:15 +0800

    refine code

--- core/circuitbreaker/circuit_breaker.go
+++ core/circuitbreaker/circuit_breaker.go
@@ -290,7 +290,7 @@ func (b *slowRtCircuitBreaker) OnRequestComplete(rt uint64, err error) {
 		return
 	}
 
-	if slowRatio > b.maxSlowRequestRatio {
+	if slowRatio > b.maxSlowRequestRatio || util.Float64Equals(slowRatio, b.maxSlowRequestRatio) {
 		curStatus = b.CurrentState()
 		switch curStatus {
 		case Closed:
@@ -468,7 +468,7 @@ func (b *errorRatioCircuitBreaker) OnRequestComplete(rt uint64, err error) {
 	if totalCount < b.minRequestAmount {
 		return
 	}
-	if errorRatio >= b.errorRatioThreshold {
+	if errorRatio > b.errorRatioThreshold || util.Float64Equals(errorRatio, b.errorRatioThreshold) {
 		curStatus = b.CurrentState()
 		switch curStatus {
 		case Closed:
