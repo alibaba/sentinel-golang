f12179b5a03172fdbeb8e600720c39e2f5f68d40
louyuting <1849491904@qq.com>
2020-11-19 09:50:18 +0800

    Refine benchmark cases (#308)

---
+++ tests/benchmark/entry/entry_benchmark_concurrency_test.go
@@ -0,0 +1,113 @@
+package entry
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"testing"
+
+	sentinel "github.com/alibaba/sentinel-golang/api"
+)
+
+func doSomethingWithSize() {
+	numbers := make([]int, 0, 200)
+	for i := 0; i < 200; i++ {
+		numbers = append(numbers, rand.Int())
+	}
+	sort.Ints(numbers)
+	//rand.Shuffle(len(numbers), func(i, j int) { numbers[i], numbers[j] = numbers[j], numbers[i] })
+}
+
+func doSomethingWithSentinelForConcurrency() {
+	e, b := sentinel.Entry("benchmark_stat_entry")
+	if b != nil {
+		fmt.Println("Blocked")
+	} else {
+		doSomethingWithSize()
+		e.Exit()
+	}
+}
+
+func Benchmark_Concurrency_Directly_4(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.SetParallelism(4)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doSomethingWithSize()
+		}
+	})
+}
+func Benchmark_Concurrency_StatEntry_4(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.SetParallelism(4)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doSomethingWithSentinelForConcurrency()
+		}
+	})
+}
+
+func Benchmark_Concurrency_Directly_8(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.SetParallelism(8)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doSomethingWithSize()
+		}
+	})
+}
+func Benchmark_Concurrency_StatEntry_8(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.SetParallelism(8)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doSomethingWithSentinelForConcurrency()
+		}
+	})
+}
+
+func Benchmark_Concurrency_Directly_16(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.SetParallelism(16)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doSomethingWithSize()
+		}
+	})
+}
+func Benchmark_Concurrency_StatEntry_16(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.SetParallelism(16)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doSomethingWithSentinelForConcurrency()
+		}
+	})
+}
+
+func Benchmark_Concurrency_Directly_32(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.SetParallelism(32)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doSomethingWithSize()
+		}
+	})
+}
+func Benchmark_Concurrency_StatEntry_32(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.SetParallelism(32)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			doSomethingWithSentinelForConcurrency()
+		}
+	})
+}
--- tests/benchmark/memory/memory_test.go
+++ tests/benchmark/memory/memory_test.go
@@ -1,4 +1,4 @@
-package tests
+package memory
 
 import (
 	"fmt"
