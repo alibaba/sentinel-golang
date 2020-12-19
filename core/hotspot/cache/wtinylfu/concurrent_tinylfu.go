69b9dd66cb553a9a660b6afc5840032c6da9ebd5
liqiangz <liqiang.zjhz@gmail.com>
2020-12-19 12:49:16 +0800

    refine

--- core/hotspot/cache/wtinylfu/concurrent_tinylfu.go
+++ core/hotspot/cache/wtinylfu/concurrent_tinylfu.go
@@ -10,20 +10,20 @@ import (
 type TinyLfuCacheMap struct {
 	// Not thread safe
 	tinyLfu *TinyLfu
-	lock    *sync.RWMutex
+	sync.RWMutex
 }
 
 func (c *TinyLfuCacheMap) Add(key interface{}, value *int64) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
+	c.Lock()
+	defer c.Unlock()
 
 	c.tinyLfu.Add(key, value)
 	return
 }
 
 func (c *TinyLfuCacheMap) AddIfAbsent(key interface{}, value *int64) (priorValue *int64) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
+	c.Lock()
+	defer c.Unlock()
 	val := c.tinyLfu.AddIfAbsent(key, value)
 	if val == nil {
 		return nil
@@ -33,8 +33,8 @@ func (c *TinyLfuCacheMap) AddIfAbsent(key interface{}, value *int64) (priorValue
 }
 
 func (c *TinyLfuCacheMap) Get(key interface{}) (value *int64, isFound bool) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
+	c.Lock()
+	defer c.Unlock()
 
 	val, found := c.tinyLfu.Get(key)
 	if found {
@@ -44,36 +44,36 @@ func (c *TinyLfuCacheMap) Get(key interface{}) (value *int64, isFound bool) {
 }
 
 func (c *TinyLfuCacheMap) Remove(key interface{}) (isFound bool) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
+	c.Lock()
+	defer c.Unlock()
 
 	return c.tinyLfu.Remove(key)
 }
 
 func (c *TinyLfuCacheMap) Contains(key interface{}) (ok bool) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
+	c.RLock()
+	defer c.RUnlock()
 
 	return c.tinyLfu.Contains(key)
 }
 
 func (c *TinyLfuCacheMap) Keys() []interface{} {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
+	c.RLock()
+	defer c.RUnlock()
 
 	return c.tinyLfu.Keys()
 }
 
 func (c *TinyLfuCacheMap) Len() int {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
+	c.RLock()
+	defer c.RUnlock()
 
 	return c.tinyLfu.Len()
 }
 
 func (c *TinyLfuCacheMap) Purge() {
-	c.lock.Lock()
-	defer c.lock.Unlock()
+	c.Lock()
+	defer c.Unlock()
 
 	c.tinyLfu.Purge()
 }
@@ -85,6 +85,5 @@ func NewTinyLfuCacheMap(size int) cache.ConcurrentCounterCache {
 	}
 	return &TinyLfuCacheMap{
 		tinyLfu: tinyLfu,
-		lock:    new(sync.RWMutex),
 	}
 }
