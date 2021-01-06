7c3ab9c49b93b875508c8926b3fe279c4f07c251
liqiangz <liqiang.zjhz@gmail.com>
2021-01-06 19:46:35 +0800

    fix ci

--- core/stat/base/metric_bucket_test.go
+++ core/stat/base/metric_bucket_test.go
@@ -27,7 +27,7 @@ func Test_metricBucket_MemSize(t *testing.T) {
 	mb := NewMetricBucket()
 	t.Log("mb:", mb)
 	size := unsafe.Sizeof(*mb)
-	if size != 64 {
+	if size != 56 {
 		t.Error("unexpect memory size of MetricBucket")
 	}
 }
