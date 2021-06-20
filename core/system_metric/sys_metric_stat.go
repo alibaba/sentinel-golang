a7dd99ce08d9ba6775c36268cdd28449dd6fe57a
hobbybear <18881934641@163.com>
2021-06-20 17:07:37 +0800

    docs: fix docs

--- core/system_metric/sys_metric_stat.go
+++ core/system_metric/sys_metric_stat.go
@@ -165,7 +165,7 @@ func retrieveAndUpdateCpuStat() {
 	currentCpuUsage.Store(cpuPercent)
 }
 
-// getProcessCpuStat gets current process's memory usage in Bytes
+// getProcessCpuStat gets current process's cpu usage in Bytes
 func getProcessCpuStat() (float64, error) {
 	curProcess := currentProcess.Load()
 	if curProcess == nil {
