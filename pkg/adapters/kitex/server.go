a24799637344212ae1740df283e0b113c16138c4
kinggo <1963359402@qq.com>
2022-07-28 11:01:17 +0800

    Add CloudWeGo Kitex adapter (#473)

---
+++ pkg/adapters/kitex/server.go
@@ -0,0 +1,36 @@
+package kitex
+
+import (
+	"context"
+
+	sentinel "github.com/alibaba/sentinel-golang/api"
+	"github.com/alibaba/sentinel-golang/core/base"
+	"github.com/cloudwego/kitex/pkg/endpoint"
+)
+
+// SentinelServerMiddleware returns new server.Middleware
+// Default resource name is {service's name}:{method}
+// Default block fallback is returning blockError
+// Define your own behavior by setting serverOptions
+func SentinelServerMiddleware(opts ...Option) func(endpoint.Endpoint) endpoint.Endpoint {
+	options := newOptions(opts)
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, req, resp interface{}) error {
+			resourceName := options.ResourceExtract(ctx, req, resp)
+			entry, blockErr := sentinel.Entry(
+				resourceName,
+				sentinel.WithResourceType(base.ResTypeRPC),
+				sentinel.WithTrafficType(base.Inbound),
+			)
+			if blockErr != nil {
+				return options.BlockFallback(ctx, req, resp, blockErr)
+			}
+			defer entry.Exit()
+			err := next(ctx, req, resp)
+			if err != nil {
+				sentinel.TraceError(entry, err)
+			}
+			return err
+		}
+	}
+}
