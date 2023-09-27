6577ac6f6d351cac28fcd8456a8586ad8132d43b
saicaca <zephyird@gmail.com>
2023-09-27 20:49:04 +0800

    feat: enhance the gRPC adapter

--- pkg/adapters/grpc/traffic.go
+++ pkg/adapters/grpc/traffic.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/alibaba/sentinel-golang/core/route"
@@ -16,6 +17,8 @@ var (
 	cm            *route.ClusterManager        = nil
 )
 
+const BAGGAGE_KEY = "_sentinel_baggage"
+
 type Baggage map[string]string
 
 func NewDialer(b Baggage) func(context.Context, string) (net.Conn, error) {
@@ -24,10 +27,19 @@ func NewDialer(b Baggage) func(context.Context, string) (net.Conn, error) {
 		if len(parts) != 2 {
 			return nil, errors.New("invalid address format")
 		}
+
+		baggageStr := ctx.Value(BAGGAGE_KEY).(string)
+		var baggage Baggage
+		err := json.Unmarshal([]byte(baggageStr), &baggage)
+		if err != nil {
+			return nil, err
+		}
+
 		tc := &base.TrafficContext{
 			ServiceName: parts[0],
 			MethodName:  parts[1],
 			Headers:     make(map[string]string),
+			Baggage:     baggage,
 		}
 
 		instance, err := cm.GetOne(tc)
@@ -51,8 +63,11 @@ func NewDialer(b Baggage) func(context.Context, string) (net.Conn, error) {
 func NewTrafficUnaryIntercepter(baggage Baggage) grpc.DialOption {
 	return grpc.WithUnaryInterceptor(
 		func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-			newCtx := ctx
-			// TODO modify the request with baggage
+			baggageStr, err := json.Marshal(baggage)
+			if err != nil {
+				return err
+			}
+			newCtx := context.WithValue(ctx, BAGGAGE_KEY, baggageStr)
 			return invoker(newCtx, method, req, reply, cc, opts...)
 		})
 }
@@ -60,16 +75,27 @@ func NewTrafficUnaryIntercepter(baggage Baggage) grpc.DialOption {
 func NewTrafficStreamIntercepter(baggage Baggage) grpc.DialOption {
 	return grpc.WithStreamInterceptor(
 		func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
-			newCtx := ctx
-			// TODO modify the request with baggage
+			baggageStr, err := json.Marshal(baggage)
+			if err != nil {
+				return nil, err
+			}
+			newCtx := context.WithValue(ctx, BAGGAGE_KEY, baggageStr)
 			return streamer(newCtx, desc, cc, method, opts...)
 		})
 }
 
+// Dial function provides a easy way to enable sentinel traffic routing. Just need to replace grpc.Dial with this function.
 func Dial(addr string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	var b Baggage = make(map[string]string)
 	opts = append(opts, grpc.WithContextDialer(NewDialer(b)))
 	opts = append(opts, NewTrafficUnaryIntercepter(b))
 	opts = append(opts, NewTrafficStreamIntercepter(b))
 	return grpc.Dial(addr, opts...)
+}
+
+// GetDialerAndIntercepter provides another way to enable sentinel traffic routing when user don't want to replace grpc.Dial.
+// User can use this function to get dialer and intercepter, and then use in grpc.Dial.
+func GetDialerAndIntercepter() (grpc.DialOption, grpc.DialOption, grpc.DialOption) {
+	var b Baggage = make(map[string]string)
+	return grpc.WithContextDialer(NewDialer(b)), NewTrafficUnaryIntercepter(b), NewTrafficStreamIntercepter(b)
 }
